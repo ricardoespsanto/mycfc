93420dc512ec3aea6afd0c17eee150dafa64cb17
Ricardo Espirito Santo <ricardoespsanto@gmail.com>
2026-07-25 22:03:56 +0100

    Backfill local consent URL defaults

--- scripts/local-bootstrap.sh
+++ scripts/local-bootstrap.sh
@@ -11,6 +11,17 @@ set -a
 source .env
 set +a
 
+for consent_url in \
+  "CONSENT_TERMS_URL=$BASE_URL/legal/termos-gerais" \
+  "CONSENT_IMAGE_URL=$BASE_URL/legal/uso-imagem" \
+  "CONSENT_MINOR_URL=$BASE_URL/legal/responsabilidade-menor"; do
+  key=${consent_url%%=*}
+  if [[ -z ${!key:-} ]]; then
+    printf '\n%s\n' "$consent_url" >> .env
+    export "$consent_url"
+  fi
+done
+
 docker compose up -d --wait postgres minio
 docker compose run --rm minio-init
 if ! docker compose exec -T postgres psql -U "$POSTGRES_USER" -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='mycfc_test'" | grep -qx 1; then
