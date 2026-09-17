b3e75fa8ada3f690f166817bdbbcb47f16f1ea9b
Ricardo <ricardoespsanto@gmail.com>
2026-09-17 12:02:48 +0100

    Fix production S3 bucket policy conditions

--- infra/environments/production/privacy_operation_receipts.tf
+++ infra/environments/production/privacy_operation_receipts.tf
@@ -165,22 +165,6 @@ data "aws_iam_policy_document" "privacy_operation_receipts_bucket" {
   }
 
   statement {
-    sid       = "DenyPutWithoutSHA256Checksum"
-    effect    = "Deny"
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.privacy_operation_receipts[0].arn}/*"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "Null"
-      variable = "s3:x-amz-checksum-sha256"
-      values   = ["true"]
-    }
-  }
-
-  statement {
     sid       = "DenyOverwriteCapablePut"
     effect    = "Deny"
     actions   = ["s3:PutObject"]
